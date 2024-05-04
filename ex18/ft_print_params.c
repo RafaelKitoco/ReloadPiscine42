8390cbcfc6edc94125f68c5983bd7ec6193e9eaf
rpmabo <rkitoco@gmail.com>
2024-05-04 14:32:57 +0100

    ex18

---
+++ ex18/ft_print_params.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpambo <rpambo@student.42luanda.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/04 14:25:24 by rpambo            #+#    #+#             */
+/*   Updated: 2024/05/04 14:32:25 by rpambo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_param(int argc, char *argv[])
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			ft_putchar(argv[i][j]);
+			j++;
+		}
+		i++;
+		ft_putchar('\n');
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	ft_param(argc, argv);
+	return (0);
+}
